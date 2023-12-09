263bfca5ddcc1191b491ae34141716bab15b2d0d
koraykoksal <kksl.koray@gmail.com>
2023-12-10 01:39:16 +0300

    up

--- v1/src/pages/Home.jsx
+++ v1/src/pages/Home.jsx
@@ -1,8 +1,11 @@
-import { Box, Button, Container } from '@mui/material'
+import { Box, Button, Card, CardContent, CardMedia, Container, Typography } from '@mui/material'
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { homeBgPattern } from '../styles/theme';
 import { useNavigate } from "react-router-dom"
+import thanks_img from "../assets/img/thanks_img.png"
+import feedback_img from "../assets/img/feedback_img.png"
+import complaint_img from "../assets/img/complaint_img.png"
 
 export const Home = () => {
 
@@ -16,19 +19,41 @@ export const Home = () => {
       <Box sx={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', py: 5, gap: 5 }}>
 
 
-        <Box sx={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', p: 5, gap: 5 }}>
+        <Card sx={{maxWidth:'350px',boxShadow:'none'}}>
+          <CardMedia
+          component='img'
+          image={thanks_img}
+          height='194'
+          sx={{objectFit:'cover',width:'100%'}}
+          />
+          <CardContent>
+          <Button fullWidth variant='outlined' color='primary' sx={{ textTransform: 'none', fontSize: '26px' }} onClick={() => navigate('/tesekkur')}>Teşekkür</Button>
+          </CardContent>
+        </Card>
 
+        <Card sx={{maxWidth:'350px',boxShadow:'none'}}>
+          <CardMedia
+          component='img'
+          image={feedback_img}
+          height='194'
+          sx={{objectFit:'cover',width:'100%'}}
+          />
+          <CardContent>
+          <Button fullWidth variant='outlined' color='secondary' sx={{ textTransform: 'none', fontSize: '26px' }} onClick={() => navigate('/oneritalep')}>Öneri & Talep</Button>
+          </CardContent>
+        </Card>
 
-          <Box sx={{ display: 'flex', flexWrap: 'wrap', p: 5, gap: 5,alignItems:'center' }}>
-
-            <Button variant='outlined' color='primary' sx={{ width: '350px', height: '100px', textTransform: 'none', fontSize: '26px' }} onClick={() => navigate('/tesekkur')}>Teşekkür</Button>
-
-            <Button variant='outlined' color='secondary' sx={{ width: '350px', height: '100px', textTransform: 'none', fontSize: '26px' }} onClick={() => navigate('/oneritalep')}>Öneri & Talep</Button>
-
-            <Button variant='outlined' color='warning' sx={{ width: '350px', height: '100px', textTransform: 'none', fontSize: '26px' }} onClick={() => navigate('/sikayet')}>Şikayet</Button>
-          </Box>
-
-        </Box>
+        <Card sx={{maxWidth:'350px',boxShadow:'none'}}>
+          <CardMedia
+          component='img'
+          image={complaint_img}
+          height='194'
+          sx={{objectFit:'cover',width:'1005'}}
+          />
+          <CardContent>
+          <Button fullWidth variant='outlined' color='warning' sx={{textTransform: 'none', fontSize: '26px' }} onClick={() => navigate('/sikayet')}>Şikayet</Button>
+          </CardContent>
+        </Card>
 
 
       </Box>
